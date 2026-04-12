b35747e9cfdb83d0aeb52cbb6bf772fb1b7b0b41
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2026-04-12 13:17:30 +0300

    Add test helpers

--- main.go
+++ main.go
@@ -1,5 +1,48 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Pattern struct {
+	x     int
+	y     int
+	cells [][]string
+}
+
+func (pattern *Pattern) toString() string {
+	var sb strings.Builder
+
+	for _, row := range pattern.cells {
+		for _, col := range row {
+			sb.WriteString(col)
+		}
+		sb.WriteString("\n")
+	}
+	return sb.String()
+}
+
+func ParseRleFile(path string) Pattern {
+	_, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file. Exiting... %v", err)
+		os.Exit(1)
+	}
+
+	fakePattern := Pattern{
+		x: 2,
+		y: 2,
+		cells: [][]string{
+			{"#", "#"},
+			{"#", "#"},
+		},
+	}
+
+	return fakePattern
+}
+
 func main() {
 	println("hello world")
 }
---
+++ main_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func assertPatternsEqual(t testing.TB, want string, got string) {
+	t.Helper()
+	if strings.TrimSpace(want) != strings.TrimSpace(got) {
+		t.Fatalf("Patterns are not equal, wanted %q, got %q", want, got)
+	}
+}
+
+func TestParseRleFile(t *testing.T) {
+	t.Run("test parsing", func(t *testing.T) {
+		blockPath := filepath.Join("patterns", "block.rle")
+		parsed := ParseRleFile(blockPath)
+
+		want := "##\n##"
+		assertPatternsEqual(t, want, parsed.toString())
+	})
+
+}
+
+func TestParseRleFileCrash(t *testing.T) {
+	if os.Getenv("BE_CRASHER") == "1" {
+		ParseRleFile("invalidpath")
+		return
+	}
+	t.Run("Test program exits when invalid path", func(t *testing.T) {
+		cmd := exec.Command(os.Args[0], "-test.run=TestParseRleFileCrash")
+		cmd.Env = append(os.Environ(), "BE_CRASHER=1")
+		err := cmd.Run()
+		if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+			return
+		}
+		t.Fatalf("process ran with err %v, want exit status 1", err)
+	})
+}
