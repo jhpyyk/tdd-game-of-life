4c05fbbaafe92b3c79b8a974691f921f33b2c9b9
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2026-04-12 17:56:30 +0300

    Refactor pattern parser test

--- pattern_parser/pattern_parser_test.go
+++ pattern_parser/pattern_parser_test.go
@@ -7,20 +7,31 @@ import (
 )
 
 func TestPatternParser(t *testing.T) {
-	t.Run("test pattern parsing", func(t *testing.T) {
+	type TestCase struct {
+		name    string
+		x       int
+		y       int
+		pattern string
+		want    string
+	}
+	testCases := []TestCase{
+		{"block", 2, 2, "2o$2o!", "##\n##\n"},
+		// {"beehive", 4, 3, "b2ob$o2bo$b2o!"},
+		// {"glider gun", 36, 9, "24bo11b$22bobo11b$12b2o6b2o12b2o$11bo3bo4b2o12b2o$2o8bo5bo3b2o14b$2o8bo3bob2o4bobo11b$10bo5bo7bo11b$11bo3bo20b$12b2o!"},
+	}
+	for _, testCase := range testCases {
+		t.Run("test pattern parsing", func(t *testing.T) {
 
-		block := "2o$2o!"
-		want := "##\n##\n"
+			pattern, err := parser.ParsePattern(testCase.x, testCase.y, testCase.pattern)
+			if err != nil {
+				t.Fatal(err.Error())
+			}
 
-		pattern, err := parser.ParsePattern(2, 2, block)
-		if err != nil {
-			t.Fatal(err.Error())
-		}
+			got := pattern.ToString()
+			if got != testCase.want {
+				t.Fatalf("Parser failed to parse pattern, want %q, got %q", testCase.want, got)
+			}
 
-		got := pattern.ToString()
-		if got != want {
-			t.Fatalf("Parser failed to parse pattern, want %q, got %q", want, got)
-		}
-
-	})
+		})
+	}
 }
