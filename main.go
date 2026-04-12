48147692ad234ab5c6c7be12f24d1d9fecd9b384
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2026-04-12 15:38:39 +0300

    Refactor parsing

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -25,52 +26,69 @@ func ParseRleFile(path string) RawPattern {
 	defer file.Close()
 
 	pat := RawPattern{}
-	var sb strings.Builder
-	var patternStartsOnNextLine bool
 
 	lines := readLines(file)
+	headerIndex, err := findHeaderLineIndex(lines)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error parsing file: %v. Exiting...", err)
+		os.Exit(1)
+	}
 
+	xdim, ydim, err := parseDimensionsFromHeader(lines[headerIndex])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error parsing file: %v. Exiting...", err)
+		os.Exit(1)
+	}
+	pat.x = xdim
+	pat.y = ydim
+
+	patternString := parsePatternString(lines[headerIndex+1:])
+	pat.patternString = patternString
+	return pat
+}
+
+func parsePatternString(lines []string) string {
+	var sb strings.Builder
 	for _, line := range lines {
 
-		var trimmedLine string
-		var lastLine bool
+		trimmedLine := strings.TrimSpace(line)
 
-		trimmedLine = strings.TrimSpace(line)
-
-		if trimmedLine[len(trimmedLine)-1] == '!' {
-			lastLine = true
-			trimmedLine = strings.Trim(trimmedLine, "!")
-		}
-
-		if patternStartsOnNextLine {
-			sb.WriteString(trimmedLine)
-		}
-
-		if trimmedLine[0] == 'x' {
-			patternStartsOnNextLine = true
-			splitted := strings.Split(trimmedLine, ",")
-			xdimString := stripRegex(splitted[0])
-			xdim, err := strconv.Atoi(xdimString)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error parsing x dimension from file %v. Exiting... %v", xdim, err)
-				os.Exit(1)
-			}
-			ydimString := stripRegex(splitted[1])
-			ydim, err := strconv.Atoi(ydimString)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error parsing y dimension from file %v. Exiting... %v", ydim, err)
-				os.Exit(1)
-			}
-			pat.x = xdim
-			pat.y = ydim
-		}
-
-		if lastLine {
-			pat.patternString = sb.String()
+		if isLastLine(trimmedLine) {
+			lineWithoutExclamation := strings.Trim(trimmedLine, "!")
+			sb.WriteString(lineWithoutExclamation)
 			break
 		}
 	}
-	return pat
+	return sb.String()
+}
+
+func parseDimensionsFromHeader(header string) (int, int, error) {
+	splitted := strings.Split(header, ",")
+	xdimString := stripRegex(splitted[0])
+	xdim, err := strconv.Atoi(xdimString)
+	if err != nil {
+		return 0, 0, fmt.Errorf("Error parsing x dimension from file %v. Exiting... %v", xdim, err)
+	}
+	ydimString := stripRegex(splitted[1])
+	ydim, err := strconv.Atoi(ydimString)
+	if err != nil {
+		return 0, 0, fmt.Errorf("Error parsing y dimension from file %v. Exiting... %v", ydim, err)
+	}
+	return xdim, ydim, nil
+}
+
+func findHeaderLineIndex(lines []string) (int, error) {
+	for i, line := range lines {
+		trimmedLine := strings.TrimSpace(line)
+		if trimmedLine[0] == 'x' {
+			return i, nil
+		}
+	}
+	return 0, errors.New("Cannot parse header")
+}
+
+func isLastLine(line string) bool {
+	return strings.Contains(line, "!")
 }
 
 func readLines(file *os.File) []string {
